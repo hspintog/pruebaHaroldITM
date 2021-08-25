6783ce953fa854a4af5add39ef14e03a71c3380d
HAROLD STIVEN PINTO GOMEZ <haroldpinto241286@correo.itm.edu.co>
2021-08-24 23:19:23 -0500

    Update Todo

---
+++ pruebaHaroldITM.Functions/Functions/TodoApi.cs
@@ -0,0 +1,114 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage.Table;
+using pruebaHaroldITM.Common.Models;
+using pruebaHaroldITM.Common.Responses;
+using pruebaHaroldITM.Functions.Entities;
+
+namespace pruebaHaroldITM.Functions.Functions
+{
+    public static class TodoApi
+    {
+        [FunctionName(nameof(CreateTodo))]
+        public static async Task<IActionResult> CreateTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous,  "post", Route = "todo")] HttpRequest req,
+            [Table("todo", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            log.LogInformation("Recieved a new todo");
+
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            Todo todo = JsonConvert.DeserializeObject<Todo>(requestBody);
+
+            if (string.IsNullOrEmpty(todo?.TaskDecription)) {
+                return new BadRequestObjectResult(new Response { IsSuccess = false, Message = "the request must have  a TaskDescription." }); 
+            }
+
+            TodoEntity todoEntity  = new TodoEntity
+            {
+                CreatedTime = DateTime.UtcNow,
+                ETag = "*",
+                IsCompleted = false,
+                PartitionKey = "TODO",
+                RowKey = Guid.NewGuid().ToString(),
+                TaskDescription = todo.TaskDecription
+            };
+
+            TableOperation addOperation = TableOperation.Insert(todoEntity);
+            await todoTable.ExecuteAsync(addOperation);
+
+            string message = "New Todo stored in table";
+            log.LogInformation(message);
+
+
+            return new OkObjectResult(new Response { 
+                IsSuccess = true,
+                Message = message,
+                Result = todoEntity
+            });
+        }
+
+
+
+
+
+
+        [FunctionName(nameof(UpdateTodo))]
+        public static async Task<IActionResult> UpdateTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todo/{id}")] HttpRequest req,
+            [Table("todo", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            string id,
+            ILogger log)
+        {
+            log.LogInformation($"Update for todo: {id}, received.");
+
+            
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            Todo todo = JsonConvert.DeserializeObject<Todo>(requestBody);
+
+            //Validate todo id
+            TableOperation findOperation = TableOperation.Retrieve<TodoEntity>("TODO", id);
+            TableResult findResult = await todoTable.ExecuteAsync(findOperation);
+
+            if(findResult.Result == null)
+            {
+                return new BadRequestObjectResult(new Response { 
+                    IsSuccess = false, 
+                    Message = "Todo not found." });
+            }
+
+
+            //Update Todo
+            TodoEntity todoEntity = (TodoEntity)findResult.Result;
+            todoEntity.IsCompleted = todo.IsCompleted;
+            if (!string.IsNullOrEmpty(todo.TaskDecription))
+            {
+                todoEntity.TaskDescription = todo.TaskDecription;
+            }
+
+
+            TableOperation addOperation = TableOperation.Replace(todoEntity);
+            await todoTable.ExecuteAsync(addOperation);
+
+            string message = $"Todo: {id}, update in table.";
+            log.LogInformation(message);
+
+
+            return new OkObjectResult(new Response
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = todoEntity
+            });
+        }
+
+    }
+}
