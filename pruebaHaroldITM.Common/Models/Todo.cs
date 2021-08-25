6323a46086c095b103a1b2b0ad9402891bb4f5e9
HAROLD STIVEN PINTO GOMEZ <haroldpinto241286@correo.itm.edu.co>
2021-08-24 22:25:25 -0500

    add entity

---
+++ pruebaHaroldITM.Common/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pruebaHaroldITM.Common.Models
+{
+    public  class Todo
+    {
+        public DateTime CreateTime { get; set; }
+
+        public string TaskDecription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
