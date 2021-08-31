335ebef7f9a9990e8ddc00b08c71961b611900a8
Harold Stiven Pinto Gomez <hpinto@bancolombia.com.co>
2021-08-31 15:24:00 -0500

    Unit Test Preparation

---
+++ pruebaHaroldITM.Test/Helpers/LoggerTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pruebaHaroldITM.Test.Helpers
+{
+    public  enum LoggerTypes
+    {
+        Null,
+        List
+    }
+}
