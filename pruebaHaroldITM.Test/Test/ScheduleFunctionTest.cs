a12be697801427be6caefe7bbdd235a7612aa075
Harold Stiven Pinto Gomez <hpinto@bancolombia.com.co>
2021-08-31 19:56:31 -0500

    add test unit Schedule and update

---
+++ pruebaHaroldITM.Test/Test/ScheduleFunctionTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using pruebaHaroldITM.Functions.Functions;
+using pruebaHaroldITM.Test.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace pruebaHaroldITM.Test.Test
+{
+    public class ScheduleFunctionTest
+    {
+        
+        [Fact]
+        public void ScheduleFunction_Should_Log_Message()
+        {
+            //arrenge
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+            //act
+            ScheduledFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+            //assert
+            Assert.Contains("Deleting completed", message);
+        }
+    }
+}
--- pruebaHaroldITM.Test/Test/TodoApiTest.cs
+++ pruebaHaroldITM.Test/Test/TodoApiTest.cs
@@ -32,5 +32,24 @@ namespace pruebaHaroldITM.Test.Test
             Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
 
         }
+
+
+        [Fact]
+        public async void UpdateTodo_Should_Return_200()
+        {
+            //Arrenge
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Todo todoRequest = TestFactory.GetTodoRequest();
+            Guid todoId = Guid.NewGuid();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(todoId,todoRequest);
+
+            //Act
+            IActionResult response = await TodoApi.UpdateTodo(request, mockTodos, todoId.ToString(), logger);
+
+            //Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+
+        }
     }
 }
